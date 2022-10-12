646cdd2ae1062e45d6f50043f47a3adc15468f8a
Jayesh Gatfane <103727724+jayeshgatfane@users.noreply.github.com>
2022-10-12 20:04:44 +0530

    Create multithreading.java

---
+++ multithreading.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+class MultithreadExm implements Runnable {  
+    public void run()  
+    {  
+        try {  
+            // It shows that thread is running  
+            System.out.println(  
+                "The thread " + Thread.currentThread().getId()  
+                + " is currently running");  
+        }  
+        catch (Exception e) {  
+            System.out.println("Catching exception");  
+        }  
+    }  
+}  
+class multithrdexp {  
+ public static void main(String[] args)  
+ {  
+     int n = 5; // Number of threads  
+     for (int i = 0; i < n; i++) {  
+         Thread object  
+             = new Thread(new MultithreadExm());  
+         object.start();  
+     }  
+ }  
+}
