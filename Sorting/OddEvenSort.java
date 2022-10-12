5bf011de87d582c71eeff55b2fe1b5dbf0665b50
Klaywert <klaywert.souza@ccc.ufcg.edu.br>
2022-10-12 00:33:43 -0300

    Add OddEven Sort

---
+++ Sorting/OddEvenSort.java
@@ -0,0 +1,25 @@
+public void oddevenSort(int[] array) {
+    boolean sorted = false;
+    while (!sorted) {
+         sorted = true;
+         for (int i = 1; i < array.length - 1; i += 2) {
+              if (array[ i ] > array[ i + 1 ]) {
+                   swapKeys( array , i , i + 1 );
+                   sorted = false;
+              }
+         }
+         for (int i = 0; i < array.length - 1; i += 2) {
+              if (array[ i ] > array[ i + 1 ]) {
+                   swapKeys( array , i , i + 1 );
+                   sorted = false;
+              }
+         }
+    }
+}
+
+public void swapKeys(int[] array, int i, int j) {
+    int temp;
+    temp = array[ i ];
+    array[ i ] = array[ j ];
+    array[ j ] = temp;
+} 
