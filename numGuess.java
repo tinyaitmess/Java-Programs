d8c77e7a6cfe0705de7a67b5a253d66be86fe014
ameypte <69102227+ameypte@users.noreply.github.com>
2022-10-20 19:47:55 +0530

    Player can set the number of trials.
    Players can play the game at different difficulty levels.

--- numGuess.java
+++ numGuess.java
@@ -2,152 +2,61 @@
 
 import java.util.Scanner;
 
- 
+public class rough {
 
-public class GFG {
+    // Function that implements the number guessing game
 
- 
-
-    // Function that implements the
-
-    // number guessing game
-
-    public static void
-
-    guessingNumberGame()
-
+    public static void guessingNumberGame()
     {
-
-        // Scanner Class
-
         Scanner sc = new Scanner(System.in);
 
- 
-
-        // Generate the numbers
-
-        int number = 1 + (int)(100
-
-                               * Math.random());
-
- 
-
-        // Given K trials
-
-        int K = 5;
-
- 
+        // Generate random numbers
+        int number = 1 + (int) (100 * Math.random());
 
         int i, guess;
 
- 
-
-        System.out.println(
-
-            "A number is chosen"
-
-            + " between 1 to 100."
-
-            + "Guess the number"
-
-            + " within 5 trials.");
-
- 
-
-        // Iterate over K Trials
+        System.out.println("A number is chosen" + " between 1 to 100.");
+        System.out.println("Enter the number of trials: ");
+        int K = sc.nextInt();
 
         for (i = 0; i < K; i++) {
 
- 
-
-            System.out.println(
-
-                "Guess the number:");
-
- 
+            System.out.println("Guess the number:");
 
             // Take input for guessing
 
             guess = sc.nextInt();
 
- 
-
             // If the number is guessed
 
             if (number == guess) {
-
-                System.out.println(
-
-                    "Congratulations!"
-
-                    + " You guessed the number.");
-
+                System.out.println("Congratulations!\nYou guessed the number.");
                 break;
-
             }
 
-            else if (number > guess
-
-                     && i != K - 1) {
-
-                System.out.println(
-
-                    "The number is "
-
-                    + "greater than " + guess);
-
+            else if (number > guess && i != K - 1) {
+                System.out.println( "The number is greater than " + guess);
             }
 
-            else if (number < guess
-
-                     && i != K - 1) {
-
-                System.out.println(
-
-                    "The number is"
-
-                    + " less than " + guess);
-
+            else if (number < guess && i != K - 1) {
+                System.out.println("The number is" + " less than " + guess);
             }
 
         }
-
- 
-
+        sc.close();
         if (i == K) {
 
-            System.out.println(
-
-                "You have exhausted"
-
-                + " K trials.");
-
- 
-
-            System.out.println(
-
-                "The number was " + number);
+            System.out.println("You have exhausted "+K+ " trials.");
+            System.out.println("The number was " + number);
 
         }
-
+        
     }
 
- 
-
     // Driver Code
-
-    public static void
-
-    main(String arg[])
-
+    public static void main(String arg[])
     {
-
- 
-
-        // Function Call
-
         guessingNumberGame();
-
     }
 
 }
